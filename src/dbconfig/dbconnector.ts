da077587e6a8a93ab33bba72a198f55f4c207d9f
Nathalia Rus <nathaliarus@gmail.com>
2020-11-01 13:54:29 +0000

    pool, pg , db connector

--- src/dbconfig/dbconnector.ts
+++ src/dbconfig/dbconnector.ts
@@ -1,1 +1,13 @@
-// comment
+var Pool = require("pg-pool");
+
+export default new Pool({
+  database: "postgres",
+  user: "postgres",
+  password: "secret!",
+  port: 5432,
+  ssl: true,
+  max: 20, // set pool max size to 20
+  idleTimeoutMillis: 1000, // close idle clients after 1 second
+  connectionTimeoutMillis: 1000, // return an error after 1 second if connection could not be established
+  maxUses: 7500, // close (and replace) a connection after it has been used 7500 times (see below for discussion)
+});
