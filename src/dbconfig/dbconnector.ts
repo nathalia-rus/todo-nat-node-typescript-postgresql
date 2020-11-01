23a160bd86a0032795b6b989672e77f575ee0249
Nathalia Rus <nathaliarus@gmail.com>
2020-11-01 18:14:24 +0000

    RDS env variables

--- src/dbconfig/dbconnector.ts
+++ src/dbconfig/dbconnector.ts
@@ -1,11 +1,11 @@
 var Pool = require("pg-pool");
 
 export default new Pool({
-  database: process.env.DB_NAME,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  port: process.env.DB_PORT,
-  host: process.env.DB_HOST,
+  database: process.env.RDS_DB_NAME,
+  user: process.env.RDS_USERNAME,
+  password: process.env.RDS_PASSWORD,
+  port: process.env.RDS_PORT,
+  host: process.env.RDS_HOSTNAME,
   max: 20, // set pool max size to 20
   idleTimeoutMillis: 1000, // close idle clients after 1 second
   connectionTimeoutMillis: 1000, // return an error after 1 second if connection could not be established
